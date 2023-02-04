f5867047ffd5267f6fe5b43ec7f8d9592e03f1d0
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2023-02-05 00:47:27 +0200

    Updates

--- storage/shaders/vertex.glsl
+++
@@ -1,8 +0,0 @@
-varying vec2 vertexUV; // vec2(0, 0.24)
-varying vec3 vertexNormal;
-
-void main(){
-    vertexUV = uv;
-    vertexNormal = normalize(normalMatrix * normal);
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-}
