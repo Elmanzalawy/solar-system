3fec5624a9552010a0f5bc4d0816b23a59498986
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2023-02-06 03:05:30 +0200

    update camera zoom from 120 to 250

--- src/scene/camera.js
+++ src/scene/camera.js
@@ -4,7 +4,7 @@ import { dimensions } from '../util';
 class Camera{
     constructor(options){
         const camera = new PerspectiveCamera(50, dimensions().width / dimensions().height, 0.1, 99999);
-        camera.position.set(0, 120, 0);
+        camera.position.set(0, 250, 0);
         camera.up.set(0, 1, 1);
         camera.lookAt(0, 0, 0);
 
